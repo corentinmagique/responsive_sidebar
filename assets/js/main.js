bb15b412e0aa33adee8a7681074a35aae4b3c267
Corentin Ckf <pcorent1@DESKTOP-ANLKKVB.localdomain>
2021-12-13 10:07:33 +0400

    Adding fullPage.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
+/* ========== SELECTING ITEM IN NAV MENU ========== */
 let nav =  document.querySelectorAll('.nav li');
+let sections =  document.querySelectorAll('.section');
 
 function clearActive()
 {
@@ -8,12 +10,47 @@ function clearActive()
     });
 }
 
+function clearSectionActive()
+{
+    sections.forEach((item) => {
+        item.classList.remove('active');
+    });
+}
+
+function setSectionActive(item)
+{
+    item.classList.add('active');
+}
+
 nav.forEach((item) =>
 {
     item.addEventListener('click',() => {
         clearActive();
         item.classList.add('active');
-        console.log('Clicked !');
+
+        /* ========== NAVIGATION THROUGH SECTION ========== */
+        // console.log(item.id);
+        // clearSectionActive();
+        // switch(item.id)
+        // {
+        //     case 'dashboard__button':
+        //         setSectionActive(sections[0]);
+        //         break;
+        //     case 'achats__button':
+        //         setSectionActive(sections[1]);
+        //         break;
+        //     case 'stock__button':
+        //         setSectionActive(sections[2]);
+        //         break;
+        //     case 'settings__button':
+        //         setSectionActive(sections[3]);
+        //         break;
+        // }
     });
 });
+
+
+
+
+
 
